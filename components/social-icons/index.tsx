1db8ef68bec8455acc1e77f2df3708bb44fa827c
InhwanCho <whdlsghks2@gmail.com>
2024-03-15 12:31:03 +0900

    post some posts and chaged some styles

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -31,7 +31,7 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-gray-600 dark:text-gray-200 dark:hover:text-gray-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 h-${size} w-${size}`}
       />
     </a>
   )
