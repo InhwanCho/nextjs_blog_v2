b13dbc8a33bdbde61ab0257b8aec04a8d4f85595
InhwanCho <whdlsghks2@gmail.com>
2024-03-19 09:15:46 +0900

    google analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -34,7 +34,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'GTM-KPKLWGN6', // e.g. G-XXXXXXX
+      googleAnalyticsId: 'G-P5TJNF1C6G', // e.g. G-XXXXXXX
     },
   },
   newsletter: {
